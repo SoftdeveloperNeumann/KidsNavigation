9c2e90d8582581c382caba0c9a364770593052f1
Frank <f.neumann.berlin@googlemail.com>
2024-04-30 12:35:15 +0200

    Planung für Messwerterfassung

--- app/src/main/java/com/example/kidsnavigation/database/entity/Messwert.kt
+++ app/src/main/java/com/example/kidsnavigation/database/entity/Messwert.kt
@@ -1,14 +1,38 @@
 package com.example.kidsnavigation.database.entity
 
-class Messwert {
 
-
-    var name:String = ""
-    var messwert: Pair<String,Double> = Pair("",0.0)
+data class Messwert(
+    var name: String = "",
+    var messwert: Double = 0.0,
     var date: Long = 0
+) {
+    var mw_id: Long = 0
+}
 
-    companion object{
-        var kategorien: MutableList<String> = mutableListOf()
+
+sealed class Gesundheitsparameter {
+    var messwert: Messwert? = null
+
+    data object Blutdruck : Gesundheitsparameter() {
+        var messwert2: Messwert? = null
     }
+    data object Herzfrequenz : Gesundheitsparameter()
+    data object Blutzucker : Gesundheitsparameter()
+    data object Koerpertemperatur : Gesundheitsparameter()
+    data object Sauerstoffsaettigung : Gesundheitsparameter()
+    data object Atemfrequenz : Gesundheitsparameter()
+    data object BodyMassIndex : Gesundheitsparameter()
+    data object Cholesterinspiegel : Gesundheitsparameter()
+}
 
+// Alternative zur SealedClass-Variante
+enum class GesundheitsparameterE {
+    Blutdruck,
+    Herzfrequenz,
+    Blutzucker,
+    Körpertemperatur,
+    Sauerstoffsättigung,
+    Atemfrequenz,
+    BodyMassIndex,
+    Cholesterinspiegel
 }
